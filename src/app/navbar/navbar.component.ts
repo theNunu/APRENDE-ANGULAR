ee331c634432e9c6b4b0e230e895a4f6ec908620
Alexis Cepeda <alexkiller0408@hotmail.com>
2025-09-09 23:21:05 -0500

    agregando css

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive} from '@angular/router';
+
+
+
 
 
 @Component({
   selector: 'app-navbar',
-  imports: [RouterOutlet],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
